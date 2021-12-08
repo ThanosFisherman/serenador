6d60f7e9e17be69eb242fa891ff1af1031445420
Thanos Psaridis <psaridis@gmail.com>
2021-12-08 17:16:24 +0200

    PhraseBook.kt data class

--- includedBuild/serenador-plugin/src/main/kotlin/io/github/thanosfisherman/serenador/plugin/SerenadorPlugin.kt
+++ includedBuild/serenador-plugin/src/main/kotlin/io/github/thanosfisherman/serenador/plugin/SerenadorPlugin.kt
@@ -6,17 +6,6 @@ import org.gradle.api.Task
 import org.gradle.api.execution.TaskExecutionListener
 import org.gradle.api.tasks.TaskState
 
-val phrases = mapOf(
-    "Bad News" to "Build process fail oh my god you look so pale",
-    "Bad News2" to "Your project failed now it's time to serenade",
-    "Bad News3" to "Your project failed and I will now serenade",
-    "Bad News4" to "Don't git commit cause your project is like shit your project manager will be mad a bit",
-    "Cello" to "Gradle project fail fail fail oh my god EPIC fail try again tomorrow maybe you get better luck",
-    "Cello2" to "In the hall of mountain king I can see your work sink what a sudden failure for your project fa la la",
-    "Cello3" to "Sorry but your coding sucks now it's time for some guts try to ask Patrice because your project gently weeps",
-    "Good News" to "Congratulations"
-)
-
 class SerenadorPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         lel(project)
@@ -32,8 +21,9 @@ class SerenadorPlugin : Plugin<Project> {
                 if (task.name.contains("assemble")) {
                     println("assembleDebug Executed")
                     if (state.executed) {
+
                         project.exec {
-                            commandLine("say", "-v", "Good News", phrases["Good News"])
+                            commandLine("say", "-v", PhraseBook.phraseList[3].voice, PhraseBook.phraseList[3].text)
                         }
                     }
                 }
