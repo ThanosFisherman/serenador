296d3200208320401d1105cfeaa4109752c15ac2
Thanos Psaridis <psaridis@gmail.com>
2021-12-20 13:47:04 +0200

    Added speech rate for extra fun :P

--- buildLogic/serenador-plugin/src/main/kotlin/io/github/thanosfisherman/serenador/commands/linux/SpdSayLinuxCommand.kt
+++ buildLogic/serenador-plugin/src/main/kotlin/io/github/thanosfisherman/serenador/commands/linux/SpdSayLinuxCommand.kt
@@ -4,5 +4,10 @@ import io.github.thanosfisherman.serenador.commands.Command
 
 class SpdSayLinuxCommand(private val text: String, var voice: String? = null) : Command() {
     override val arguments: String
-        get() = voice?.let { "spd-say -w -t \"$it\" \"$text\"" } ?: "spd-say \"$text\""
+        get() = voice?.let { "spd-say -w -r ${randomRate()} -t \"$it\" \"$text\"" }
+            ?: "spd-say -w -r ${randomRate()} \"$text\""
+
+    private fun randomRate(): String {
+        return (-90..0).random().toString()
+    }
 }
